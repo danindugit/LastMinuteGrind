4ba8c340104e90a7338a640f76c36c43a09b64e8
bmarasi <danindum@outlook.com>
2020-12-19 02:16:25 -0500

    moved firebase init here

---
+++ public/app.js
@@ -0,0 +1,8 @@
+// Initialize Cloud Firestore through Firebase
+firebase.initializeApp({
+    apiKey: 'AIzaSyCpAJV484s7mbNiE9irNGU5njMx89no4kg',
+    authDomain: 'last-minute-grind.firebaseapp.com',
+    projectId: 'last-minute-grind'
+    });
+
+var db = firebase.firestore();
